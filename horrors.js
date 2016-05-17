ee029c6934ec578625a3021e113ea2188bbfd0af
Matthew Fister <mfister@constantcontact.com>
2016-05-17 10:14:45 -0400

    Start horrors

---
+++ horrors.js
@@ -0,0 +1,16 @@
+var horrors = {
+    "title": "Astral Horrors",
+    "description": "As a feeling of unspeakable dread washes over them, the adventurers realize that they have attracted the attention of a strange being from a distant plane or dimension.",
+    "roll": "1",
+
+    "nameStart": "Ch|X|S|Ick|Ix|Ex|Ax|Ach|Gr",
+    "nameSyl": "iz|ix|ez|az|ach|ech|ich|eb|aarg|arth|irth|erth|earth",
+    "nameStart2": "Ch|X|S|Ick|Ix|Ex|Ax|Ach|Gr",
+    "nameSyl2": "iz|ix|ez|az|ach|ech|ich|eb|aarg|arth|irth|erth|earth",
+
+    "name": "{{nameStart}}{{nameSyl}}{{nameSyl}}|{{nameStart}}{{nameSyl}}|{{nameStart}}{{nameSyl}} {{nameStart}}{{nameSyl}}|{{nameStart}}{{nameSyl}} {{nameStart2}}{{nameSyl2}}",
+
+
+    "1-midboss-title": "{{name}}",
+    "1-midboss-description": "John 'The Badger' Collins is a weretiger bounty hunter. In his left hand is 'Raygar', an enchanted orcish raygun. His right hand holds 'Delaware', an ancient stone shield."
+}
--- midboss.js
+++
@@ -1,8 +0,0 @@
-var midboss = {
-    "title": "Mid Boss",
-    "description": "This dude is worse than the other ones you've seen. A real badass glares at you.",
-    "roll": "1",
-
-    "1-midboss-title": "Mid Boss",
-    "1-midboss-description": "John 'The Badger' Collins is a weretiger bounty hunter. In his left hand is 'Raygar', an enchantd orcish raygun. His right hand holds 'Delaware', an ancient stone shield."
-}
