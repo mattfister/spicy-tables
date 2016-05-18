f213c2b229b837b916beebfeed68188eae1212f6
mattfister@gmail.com <mattfister@gmail.com>
2016-05-17 22:07:18 -0400

    Trying to convert to tracery

--- horrors.js
+++ horrors.js
@@ -1,19 +1,16 @@
-var horrors = {
+horrors = {
     "title": "Astral Horrors",
     "description": "As a feeling of unspeakable dread washes over them, the adventurers realize that they have attracted the attention of a strange being from a distant plane or dimension.",
     "roll": "1",
 
-    "nameStart": "Ch|X|S|Ick|Ix|Ex|Ax|Ach|Gr",
-    "nameSyl": "iz|ix|ez|az|ach|ech|ich|eb|aarg|arth|irth|erth|earth",
-    "nameStart2": "Ch|X|S|Ick|Ix|Ex|Ax|Ach|Gr",
-    "nameSyl2": "iz|ix|ez|az|ach|ech|ich|eb|aarg|arth|irth|erth|earth",
+    "nameStart": ["Ch","X","S","Ick","Ix","Ex","Ax","Ach","Gr"],
+    "nameSyl": ["iz","ix","ez","az","ach","ech","ich","eb","aarg","arth","irth","erth","earth"],
 
-    "name": "{{nameStart}}{{nameSyl}}{{nameSyl}}|{{nameStart}}{{nameSyl}}|{{nameStart}}{{nameSyl}} {{nameStart}}{{nameSyl}}|{{nameStart}}{{nameSyl}} {{nameStart2}}{{nameSyl2}}",
+    "name": "#nameStart##nameSyl##nameSyl#",
 
 
+    "formSentence": "#name# takes the form of a ethereal cat monster.",
 
-    "formSentence": "{{name}} takes the form of a ethereal cat monster.",
-
-    "1-midboss-title": "{{name}}",
-    "1-midboss-description": "{{formSentence}} "
+    "1-midboss-title": "An Astral Horror Appears!",
+    "1-midboss-description": "#formSentence# #name#"
 }
