cc113214d3c886ba996f8d9b4d14b4d9cd320086
Matt Fister <mattfister@gmail.com>
2016-05-18 11:33:20 -0400

    Tracery branch (#2)
    * Make image background
    
    * Trying to convert to tracery
    
    * Tracery conversion complete
    
    * Remove image and add about modal

--- horrors.js
+++ horrors.js
@@ -1,16 +1,19 @@
-var horrors = {
+horrors = {
     "title": "Astral Horrors",
     "description": "As a feeling of unspeakable dread washes over them, the adventurers realize that they have attracted the attention of a strange being from a distant plane or dimension.",
     "roll": "1",
 
-    "nameStart": "Ch|X|S|Ick|Ix|Ex|Ax|Ach|Gr",
-    "nameSyl": "iz|ix|ez|az|ach|ech|ich|eb|aarg|arth|irth|erth|earth",
-    "nameStart2": "Ch|X|S|Ick|Ix|Ex|Ax|Ach|Gr",
-    "nameSyl2": "iz|ix|ez|az|ach|ech|ich|eb|aarg|arth|irth|erth|earth",
+    "nameStart": ["Ch","X","S","Ick","Ix","Ex","Ax","Ach","Gr"],
+    "nameSyl": ["iz","ix","ez","az","ach","ech","ich","eb","aarg","arth","irth","erth","earth"],
 
-    "name": "{{nameStart}}{{nameSyl}}{{nameSyl}}|{{nameStart}}{{nameSyl}}|{{nameStart}}{{nameSyl}} {{nameStart}}{{nameSyl}}|{{nameStart}}{{nameSyl}} {{nameStart2}}{{nameSyl2}}",
+    "name": "#nameStart##nameSyl##nameSyl#",
 
 
-    "1-midboss-title": "{{name}}",
-    "1-midboss-description": "John 'The Badger' Collins is a weretiger bounty hunter. In his left hand is 'Raygar', an enchanted orcish raygun. His right hand holds 'Delaware', an ancient stone shield."
+    "formSentence": "#horror# takes the form of a ethereal cat monster.",
+    "eatsSentence": "#horror# eats children.",
+
+    "horror-description": "#formSentence# #eatsSentence#",
+
+    "1-midboss-title": "An Astral Horror Appears!",
+    "1-midboss-description": "#[horror:#name#]horror-description#"
 }
